0c9e0e7e22b84ec96c750cbf61bff09116523e59
Fred Nora <freddynora@gmail.com>
2025-01-02 17:26:54 -0300

    client.py: Sending message to Gramado OS.

--- udp/client.py
+++ udp/client.py
@@ -1,11 +1,16 @@
 import time
 from socket import *
 
+
+TARGET_IP = "192.168.1.4"
+TARGET_PORT = 11888
+
 MESSAGE = b"Hello from python script on Linux.\0"
+MESSAGE2 = b"g:4\0"
 
 pings = 1
 
-#Send ping 10 times 
+# Send ping 10 times 
 while pings < 11:  
 
     #Create a UDP socket
@@ -14,13 +19,15 @@ while pings < 11:
     #Set a timeout value of n seconds
     clientSocket.settimeout(5)
 
-    addr = ("192.168.1.4", 11888)
+    #addr = ("192.168.1.4", 11888)
+    addr = (TARGET_IP,TARGET_PORT)
 
-    #Send ping
+    # Send ping
     start = time.time()
     clientSocket.sendto(MESSAGE, addr)
+    # clientSocket.sendto(MESSAGE2, addr)
 
-    #If data is received back from server, print 
+    # If data is received back from server, print 
     try:
         data, server = clientSocket.recvfrom(1024)
         end = time.time()
